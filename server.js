05f0a2aaaaaca58ba1b0d1b973ac1f94a71abab5
ZINEEDDINE BOUMRAR <zineeddine.boumrar@gmail.com>
2024-09-05 09:44:36 +0100

    path not foand fixing commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const sequelize = require('./config/Database');
 //error handler
 const ErrorHandler = require('./controller/ErrorController');
